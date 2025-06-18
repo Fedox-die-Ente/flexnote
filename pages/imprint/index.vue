ec48f666a52f89ae3c143c094a98c450be8672c2
Florian <mrminecrafttv19@gmail.com>
2025-06-18 19:34:04 +0200

    add general information and contact details to index.vue

---
+++ pages/imprint/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<body class="bg-black-deep w-screen h-screen justify-center items-center flex">
+	<div class="bg-black text-white p-4 rounded-lg">
+		<h1 class="text-xl font-bold mb-2">
+			General Information
+		</h1>
+		<p class="mb-2">Florian Ohldag</p>
+		<p class="mb-2">c/o Autorenglück #25795</p>
+		<p class="mb-2">Address: Albert-Einstein-Straße 47, 02977 Hoyerswerda</p>
+		<h1 class="text-xl font-bold mb-2">
+			Contact Information
+		</h1>
+		<p class=" mb-2">Phone: +49 176 53744939 <span class="text-accent font-bold">*</span></p>
+		<p class=" mb-2">Email: f3dox@proton.me</p>
+		<p class="text-accent font-bold">
+			* Under this phone number, is no technical support available.
+		</p>
+		<p class="text-accent font-bold">
+			* For business inquiries, please use the email address above.
+		</p>
+	</div>
+	</body>
+</template>
